418b4babfec001857922091e7072a0bb9fd97adc
meihongwei <xiyanxiyan10@hotmail.com>
2019-07-05 17:29:47 +0800

    add log interface

---
+++ quant/server/src/github.com/xiyanxiyan10/gobacktest/log.go
@@ -0,0 +1,24 @@
+package gobacktest
+
+type BackLog interface {
+	Printf(format string, v ...interface{})
+	Print(v ...interface{})
+
+	Debugf(format string, v ...interface{})
+	Debug(v ...interface{})
+
+	Infof(format string, v ...interface{})
+	Info(v ...interface{})
+
+	Warnf(format string, v ...interface{})
+	Warn(v ...interface{})
+
+	Errorf(format string, v ...interface{})
+	Error(v ...interface{})
+
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+
+	Panic(v ...interface{})
+	Panicf(format string, v ...interface{})
+}
