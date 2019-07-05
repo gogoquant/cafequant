8e99e47e1741ac7674959f7df46270925076847a
meihongwei <xiyanxiyan10@hotmail.com>
2019-07-05 16:53:33 +0800

    better backtest test files

--- quant/server/src/github.com/xiyanxiyan10/gobacktest/backtest_test.go
+++ quant/server/src/github.com/xiyanxiyan10/gobacktest/backtest_test.go
@@ -5,25 +5,11 @@ import (
 	"testing"
 )
 
-//1 方法必须传入testing.T 2 方法名必须是以Test开头，驼峰命名
+// TestBack test backTest interface
 func TestBack(t *testing.T) {
-	fmt.Println("create new backtest")
+	fmt.Println("create new backTest")
 	backConfig := make(map[string]string)
 	back := NewBackTest(backConfig)
 
-	//@todo need scripts
-	back.SetScripts("")
-	//start back
-
-	go func() {
-		back.Start()
-	}()
-
-	//todo read data from history and put into back
-	go func() {
-
-	}()
-
-	//try to print status of the backtest
-
+	back.Next()
 }
