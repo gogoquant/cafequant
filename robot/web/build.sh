c16a388bcab504dd35a5fd2653ddad6269d5b37f
xiyanixyan10 <xiyanxiyan10@hotmail.com>
2019-11-21 15:08:40 +0800

    add build file for web

---
+++ robot/web/build.sh
@@ -0,0 +1,3 @@
+npm install -g cnpm --registry=https://registry.npm.taobao.org && cnpm install cooking -g && cnpm install cooking-cli -g 
+cd /opt/app/web && npm update && npm install 
+cooking build
