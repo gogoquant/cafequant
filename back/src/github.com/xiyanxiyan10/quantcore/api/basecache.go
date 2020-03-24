93ba4b8ae8a4172587ad7ccf6f49de51ab715af7
hongwei.mei <hongwei.mei@changhong.com>
2020-03-24 18:02:04 +0800

    add more info for cache

--- back/src/github.com/xiyanxiyan10/quantcore/api/basecache.go
+++ back/src/github.com/xiyanxiyan10/quantcore/api/basecache.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/xiyanxiyan10/quantcore/constant"
 	"sync"
+	"time"
 )
 
 // BaseExchangeCache store val as cache for callback api
@@ -10,8 +11,15 @@ type BaseExchangeCache struct {
 	BaseExchange
 
 	sync.Mutex
-	depth   constant.Depth
-	orders  []constant.Order
-	traders []constant.Trader
-	ticker  constant.Ticker
+	depth     constant.Depth
+	depthTime time.Time
+
+	orders     []constant.Order
+	ordersTime time.Time
+
+	traders     []constant.Trader
+	tradersTime time.Time
+
+	ticker     constant.Ticker
+	tickerTime time.Time
 }
