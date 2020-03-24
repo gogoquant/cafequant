813672da68c472716d56449bc4c447fdf91fc8ca
hongwei.mei <hongwei.mei@changhong.com>
2020-03-24 16:53:56 +0800

    1.delete file not used

--- back/src/github.com/xiyanxiyan10/quantcore/gobacktest/algorithm.go
+++ back/src/github.com/xiyanxiyan10/quantcore/gobacktest/algorithm.go
@@ -1,8 +1,39 @@
 package gobacktest
 
 import (
+	"errors"
 	"fmt"
 )
+
+// MetricHandler defines the handling of metrics to a data event
+type MetricHandler interface {
+	Add(string, float64) error
+	Get(string) (float64, bool)
+}
+
+// Metric holds metric propertys to a data point.
+type Metric map[string]float64
+
+// Add ads a value to the metrics map
+func (m Metric) Add(key string, value float64) error {
+	if m == nil {
+		m = make(map[string]float64)
+	}
+
+	if key == "" {
+		return errors.New("invalid key given")
+	}
+
+	m[key] = value
+	return nil
+}
+
+// Get return a metric by name, if not found it returns false.
+func (m Metric) Get(key string) (float64, bool) {
+	value, ok := m[key]
+	return value, ok
+}
+
 
 // Mean calculates the average for a slice of float64 values.
 func Mean(values []float64) float64 {
@@ -72,3 +103,4 @@ func EMA(values []float64, period int) ([]float64, error) {
 
 	return result, nil
 }
+
