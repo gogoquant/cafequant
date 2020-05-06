7fbf20066f70502397671e6f4b784d9af537611e
hongwei.mei <hongwei.mei@changhong.com>
2020-05-06 09:36:10 +0800

    catch error in handler

--- goback/src/snack.com/xiyanxiyan10/stocktrader/handler/handler.go
+++ goback/src/snack.com/xiyanxiyan10/stocktrader/handler/handler.go
@@ -67,5 +67,8 @@ func Server() {
 	http.Handle("/", http.FileServer(http.Dir(drawPath)))
 	fmt.Printf("%v  Version %v\n", constant.Banner, constant.Version)
 	log.Printf("Running at :%v\n", port)
-	http.ListenAndServe(":"+port, nil)
+	if err := http.ListenAndServe(":"+port, nil); err != nil{
+		log.Printf("Listen at %v fail:%s\n", port, err.Error())
+		return
+	}
 }
