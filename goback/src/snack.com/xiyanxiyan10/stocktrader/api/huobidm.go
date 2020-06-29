430ce936c3ca7f76a57909961515deb0d0129abd
hongwei.mei <hongwei.mei@changhong.com>
2020-06-29 13:20:46 +0800

    删除无用的代码

--- goback/src/snack.com/xiyanxiyan10/stocktrader/api/huobidm.go
+++ goback/src/snack.com/xiyanxiyan10/stocktrader/api/huobidm.go
@@ -2,31 +2,7 @@ package api
 
 import "snack.com/xiyanxiyan10/stocktrader/constant"
 
-/*
-func TestNewHbdmWs(t *testing.T) {
-	ws := NewHbdmWs()
-	ws.ProxyUrl("socks5://127.0.0.1:1080")
-
-	ws.SetCallbacks(func(ticker *goex.FutureTicker) {
-		log.Println(ticker.Ticker)
-	}, func(depth *goex.Depth) {
-		log.Println(">>>>>>>>>>>>>>>")
-		log.Println(depth.ContractType, depth.Pair)
-		log.Println(depth.BidList)
-		log.Println(depth.AskList)
-		log.Println("<<<<<<<<<<<<<<")
-	}, func(trade *goex.Trade, s string) {
-		log.Println(s, trade)
-	})
-
-	t.Log(ws.SubscribeTicker(goex.BTC_USD, goex.QUARTER_CONTRACT))
-	t.Log(ws.SubscribeDepth(goex.BTC_USD, goex.NEXT_WEEK_CONTRACT, 0))
-	t.Log(ws.SubscribeTrade(goex.LTC_USD, goex.THIS_WEEK_CONTRACT))
-	time.Sleep(time.Minute)
-}
-*/
-
-// NewFutureExchange create an exchange struct of futureExchange.com
+// NewHuoBiDmExchange create an exchange struct of futureExchange.com
 func NewHuoBiDmExchange(opt constant.Option) Exchange {
 	exchange := NewFutureExchange(opt)
 	_ = exchange.Init()
