4b1bf41b5a6e4968e653432587f487c5a9742aeb
hongwei.mei <hongwei.mei@changhong.com>
2020-08-26 17:47:15 +0800

    允许插件动态加载

--- goback/src/snack.com/xiyanxiyan10/stocktrader/goplugin/plugin.go
+++ goback/src/snack.com/xiyanxiyan10/stocktrader/goplugin/plugin.go
@@ -1,51 +1,105 @@
 package goplugin
 
 import (
+	"errors"
+	"plugin"
 	"snack.com/xiyanxiyan10/stocktrader/api"
 )
 
 // Gofunc ...
 type Gofunc func(...interface{}) interface{}
 
-// GoPlugin ...
-type GoPlugin struct {
-	goPluginGrid
+// GoStragey  ...
+type GoStragey struct {
 	exchanges []api.Exchange
-	funcs     map[string]Gofunc
 }
 
 // AddExchange ...
-func (p *GoPlugin) AddExchange(e api.Exchange) {
-	p.exchanges = append(p.exchanges, e)
+func (p *GoStragey) AddExchange(e ...api.Exchange) {
+	p.exchanges = append(p.exchanges, e...)
 }
 
-// AddFunc ...
-func (p *GoPlugin) AddFunc(name string, callback Gofunc) {
-	p.funcs[name] = callback
+// GoStrageyHandler ...
+type GoStrageyHandler interface {
+	AddExchange(...api.Exchange)
+	Init(...interface{}) interface{}
+	Call(...interface{}) interface{}
+	Exit(...interface{}) interface{}
+}
+
+// GoPlugin ...
+type GoPlugin struct {
+	exchanges []api.Exchange
+	strageys  map[string]GoStrageyHandler
+}
+
+// AddExchange ...
+func (p *GoPlugin) AddExchange(e ...api.Exchange) {
+	p.exchanges = append(p.exchanges, e...)
+}
+
+// AddStragey ...
+func (p *GoPlugin) AddStragey(name string, v GoStrageyHandler) {
+	p.strageys[name] = v
+	v.AddExchange(p.exchanges...)
+}
+
+// LoadStragey ...
+func (p *GoPlugin) LoadStragey(name string) error {
+	handler, err := plugin.Open("/tmp/" + name + ".so")
+	if err != nil {
+		return err
+	}
+	s, err := handler.Lookup("NewHandler")
+	if err != nil {
+		return err
+	}
+	v, ok := s.(GoStrageyHandler)
+	if !ok {
+		return errors.New("load stragey handler fail")
+	}
+	p.AddStragey(name, v)
+	return nil
+}
+
+// Init ...
+func (p *GoPlugin) Init(name string, v ...interface{}) interface{} {
+	handler, ok := p.strageys[name]
+	if !ok {
+		return false
+	}
+	return handler.Init(v)
 }
 
 // Call ...
 func (p *GoPlugin) Call(name string, v ...interface{}) interface{} {
-	callback, ok := p.funcs[name]
+	handler, ok := p.strageys[name]
 	if !ok {
 		return false
 	}
-	return callback(v)
+	return handler.Call(v)
+}
+
+// Exit ...
+func (p *GoPlugin) Exit(name string, v ...interface{}) interface{} {
+	handler, ok := p.strageys[name]
+	if !ok {
+		return false
+	}
+	return handler.Exit(v)
 }
 
 // Handler ...
 type Handler interface {
-	// string is the stragey name, interface is the param and return
-	Call(string, ...interface{}) interface{}
+	AddStragey(name string, v GoStrageyHandler)
+	LoadStragey(name string) error
+	Init(name string, v ...interface{}) interface{}
+	Call(name string, v ...interface{}) interface{}
+	Exit(name string, v ...interface{}) interface{}
 }
 
 // NewGoPlugin ...
 func NewGoPlugin() *GoPlugin {
 	var goplugin GoPlugin
-	goplugin.funcs = make(map[string]Gofunc)
-
-	//register go func here
-	goplugin.AddFunc("hello", goplugin.Hello)
-
 	return &goplugin
 }
