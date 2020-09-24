861c15360f6e7c60cca761633588dd8e75681e43
hongwei.mei <hongwei.mei@changhong.com>
2020-09-24 13:56:54 +0800

    1.初始化ctp接口

--- goback/src/snack.com/xiyanxiyan10/stocktrader/api/ctp.go
+++ goback/src/snack.com/xiyanxiyan10/stocktrader/api/ctp.go
@@ -4,16 +4,8 @@ import (
 	"snack.com/xiyanxiyan10/stocktrader/constant"
 )
 
-/*
-const (
-	tickerURL = "http://hq.sinajs.cn/list="
-	depthURL  = "http://hq.sinajs.cn/list="
-	recordURL = "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?"
-)
-*/
-
-// SZExchange the exchange struct of futureExchange.com
-type SZExchange struct {
+// CTPExchange the exchange struct of futureExchange.com
+type CTPExchange struct {
 	BaseExchange
 
 	tradeTypeMap        map[int]string
@@ -23,10 +15,11 @@ type SZExchange struct {
 	records map[string][]constant.Record
 }
 
-// NewSZExchange create an exchange struct of futureExchange.com
-func NewSZExchange(opt constant.Option) *SZExchange {
-	spotExchange := SZExchange{
+// NewCTPExchange create an exchange struct of futureExchange.com
+func NewCTPExchange(opt constant.Option) *CTPExchange {
+	spotExchange := CTPExchange{
 		records: make(map[string][]constant.Record),
+		//apiBuilder: builder.NewAPIBuilder().HttpTimeout(5 * time.Second),
 	}
 	spotExchange.SetRecordsPeriodMap(map[string]int64{
 		"M1":  60,
@@ -44,77 +37,77 @@ func NewSZExchange(opt constant.Option) *SZExchange {
 }
 
 // Ready ...
-func (e *SZExchange) Ready() interface{} {
+func (e *CTPExchange) Ready() interface{} {
 	return "success"
 }
 
 // Init get the type of this exchange
-func (e *SZExchange) Init(opt constant.Option) error {
+func (e *CTPExchange) Init(opt constant.Option) error {
 	e.BaseExchange.Init(opt)
 	return nil
 }
 
 // Log print something to console
-func (e *SZExchange) Log(msgs ...interface{}) {
+func (e *CTPExchange) Log(msgs ...interface{}) {
 	e.logger.Log(constant.INFO, e.GetStockType(), 0.0, 0.0, msgs...)
 }
 
 // GetType get the type of this exchange
-func (e *SZExchange) GetType() string {
+func (e *CTPExchange) GetType() string {
 	return e.option.Type
 }
 
 // GetName get the name of this exchange
-func (e *SZExchange) GetName() string {
+func (e *CTPExchange) GetName() string {
 	return e.option.Name
 }
 
 // GetDepth ...
-func (e *SZExchange) GetDepth() interface{} {
+func (e *CTPExchange) GetDepth() interface{} {
 	return nil
 }
 
 // GetPosition ...
-func (e *SZExchange) GetPosition() interface{} {
+func (e *CTPExchange) GetPosition() interface{} {
 	return nil
 }
 
 // GetAccount get the account detail of this exchange
-func (e *SZExchange) GetAccount() interface{} {
+func (e *CTPExchange) GetAccount() interface{} {
 	return nil
 }
 
 // Buy ...
-func (e *SZExchange) Buy(price, amount string, msg ...interface{}) interface{} {
+func (e *CTPExchange) Buy(price, amount string, msg ...interface{}) interface{} {
 	return nil
 }
 
 // Sell ...
-func (e *SZExchange) Sell(price, amount string, msg ...interface{}) interface{} {
+func (e *CTPExchange) Sell(price, amount string, msg ...interface{}) interface{} {
 	return nil
 }
 
 // GetOrder get details of an order
-func (e *SZExchange) GetOrder(id string) interface{} {
+func (e *CTPExchange) GetOrder(id string) interface{} {
 	return nil
 }
 
 // GetOrders get all unfilled orders
-func (e *SZExchange) GetOrders() interface{} {
+func (e *CTPExchange) GetOrders() interface{} {
 	return nil
 }
 
 // CancelOrder cancel an order
-func (e *SZExchange) CancelOrder(orderID string) interface{} {
+func (e *CTPExchange) CancelOrder(orderID string) interface{} {
 	return nil
 }
 
 // GetTicker get market ticker
-func (e *SZExchange) GetTicker() interface{} {
+func (e *CTPExchange) GetTicker() interface{} {
 	return nil
 }
 
 // GetRecords get candlestick data
-func (e *SZExchange) GetRecords(periodStr string) interface{} {
+func (e *CTPExchange) GetRecords(periodStr string) interface{} {
 	return nil
 }
