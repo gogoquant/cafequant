d958251aaf1e5568fc50837da8b0fc5976f2bd47
hongwei.mei <hongwei.mei@changhong.com>
2020-12-01 15:27:20 +0800

    fix error in ctp

--- goback/src/snack.com/xiyanxiyan10/stocktrader/sdk/ctp/ctp_test.go
+++ goback/src/snack.com/xiyanxiyan10/stocktrader/sdk/ctp/ctp_test.go
@@ -4,5 +4,9 @@ import "testing"
 
 func TestCtp(t *testing.T) {
 	ctp := NewCtp()
-	t.Log("hello world")
+	err := ctp.Start()
+	if err != nil {
+		t.Log("ctp start err:" + err.Error())
+		return
+	}
 }
