909acdcfab2b447d3a96173bee210e5bc15a6fa3
xiyanxiyan10 <xiyanxiyan10@hotmail.com>
2021-01-25 11:20:38 +0000

    add comment

--- back/src/snack.com/xiyanxiyan10/stocktrader/api/future.go
+++ back/src/snack.com/xiyanxiyan10/stocktrader/api/future.go
@@ -132,6 +132,7 @@ func (e *FutureExchange) GetStockTypeMap() map[string]goex.CurrencyPair {
 	return e.stockTypeMap
 }
 
+// load synv data from server
 func (e *FutureExchange) load() {
 	for e.loadstatus == constant.Running {
 		subscribe := e.GetSubscribe()
