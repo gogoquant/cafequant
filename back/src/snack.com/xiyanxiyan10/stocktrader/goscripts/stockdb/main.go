f7d2ced56f431baa761ec34dfed120bfd1095d2b
hongwei.mei <hongwei.mei@changhong.com>
2021-02-01 16:10:29 +0800

    demo file update

--- back/src/snack.com/xiyanxiyan10/stocktrader/goscripts/stockdb/main.go
+++ back/src/snack.com/xiyanxiyan10/stocktrader/goscripts/stockdb/main.go
@@ -6,11 +6,12 @@ import (
 	"snack.com/xiyanxiyan10/stocktrader/api"
 	"snack.com/xiyanxiyan10/stocktrader/config"
 	"snack.com/xiyanxiyan10/stocktrader/constant"
+	"snack.com/xiyanxiyan10/stocktrader/util"
 )
 
 // main ...
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Println("命令行的参数不合法:", len(os.Args))
 		return
 	}
@@ -23,6 +24,7 @@ func main() {
 	var symbol = "BTC/USD"
 	var io = "online"
 	var period = "H1"
+	var path = os.Args[2]
 
 	opt.AccessKey = ""
 	opt.SecretKey = ""
@@ -45,6 +47,13 @@ func main() {
 	exchange.SetPeriod(period)
 	exchange.SetPeriodSize(10)
 	exchange.Start()
+
+	data, err := util.ReadCSV(path)
+	if err != nil {
+		fmt.Printf("read csv fail:%s\n", err.Error())
+		return
+	}
+	fmt.Printf("%s\n", util.Struct2Json(data))
 	/*
 		exchange.SetSubscribe(symbol, constant.CacheAccount)
 		exchange.SetSubscribe(symbol, constant.CacheRecord)
