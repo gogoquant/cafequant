fb9747f5780e3223c30577aadf1114d68689d4e8
hongwei.mei <hongwei.mei@changhong.com>
2021-02-07 11:07:51 +0800

    update sz api

--- back/src/snack.com/xiyanxiyan10/stocktrader/api/spotsz.go
+++ back/src/snack.com/xiyanxiyan10/stocktrader/api/spotsz.go
@@ -20,6 +20,20 @@ const (
 	recordURL = "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?"
 )
 
+// NewSZExchange create an exchange struct of futureExchange.com
+func NewSZExchange(opt constant.Option) (Exchange, error) {
+	exchange := NewSZSpotExchange(opt)
+	exchange.SetRecordsPeriodMap(map[string]int64{
+		"M5":  5,
+		"M15": 15,
+		"M30": 30,
+	})
+	if err := exchange.Init(opt); err != nil {
+		return nil, err
+	}
+	return exchange, nil
+}
+
 // getRecords ...
 func getRecords(symbol string, period, ma, size int) (string, error) {
 	client := &http.Client{}
