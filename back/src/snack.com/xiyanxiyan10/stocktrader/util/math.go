9097965e9328b91443c5a62790bb004a35e87de5
hongwei.mei <hongwei.mei@changhong.com>
2021-03-01 10:21:33 +0800

    加入bar线合成接口

---
+++ back/src/snack.com/xiyanxiyan10/stocktrader/util/math.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"snack.com/xiyanxiyan10/stocktrader/constant"
+)
+
+type KlineMerge struct {
+	size  int
+	Limit int
+	Count int
+	curr  *constant.Record
+	vec   []constant.Record
+}
+
+func NewKlineMerge(rate int) *KlineMerge {
+	var merge KlineMerge
+	merge.Limit = rate
+	return &merge
+}
+
+func (m *KlineMerge) Append(klines ...constant.Record) {
+	for _, kline := range klines {
+		m.Count += 1
+		if m.Count > m.Limit {
+			// merge one kline and store it
+			if m.curr != nil {
+				m.vec = append(m.vec, *(m.curr))
+			}
+			m.Count = 1
+			m.curr = nil
+		}
+		if m.curr == nil {
+			m.curr = new(constant.Record)
+			*(m.curr) = kline
+			continue
+		}
+		m.curr.Volume += kline.Volume
+		m.curr.Time = kline.Time
+		m.curr.Close = kline.Close
+		if m.curr.High < kline.High {
+			m.curr.High = kline.High
+		}
+		if m.curr.Low > kline.Low {
+			m.curr.Low = kline.Low
+		}
+	}
+}
+
+func (m *KlineMerge) Get() []constant.Record {
+	return m.vec
+}
