f2523525ca4c514eb6d4b91617416f9972660d6e
hongwei.mei <hongwei.mei@changhong.com>
2021-03-10 15:26:43 +0800

    调整脚本独立运行标志的名字

--- back/src/snack.com/xiyanxiyan10/stocktrader/api/futureback.go
+++ back/src/snack.com/xiyanxiyan10/stocktrader/api/futureback.go
@@ -517,7 +517,7 @@ func (ex *ExchangeFutureBack) GetTicker(currency string) (*constant.Ticker, erro
 		curr := loader.Next()
 		if curr == nil {
 			// 独立运行的可以直接退出，外部主程序管理的脚本则抛出异常，由外部处理
-			if ex.option.Alone {
+			if ex.option.BackExit {
 				os.Exit(0)
 			}
 			panic(constant.BackEnd)
