d809b2f1ccc477d952a82d561d51b1c79574ac98
xiyanxiyan10 <873706510@qq.com>
2021-03-13 12:31:49 +0800

    better gopy build scripts

--- back/src/snack.com/xiyanxiyan10/stocktrader/pybuild.sh
+++ back/src/snack.com/xiyanxiyan10/stocktrader/pybuild.sh
@@ -1,1 +1,2 @@
-gopy build -output=out -vm=python3.7 snack.com/xiyanxiyan10/stocktrader/constant snack.com/xiyanxiyan10/stocktrader/api snack.com/xiyanxiyan10/stocktrader/config
+# export LD_LIBRARY_PATH=./
+gopy build -output=out -vm=python3 snack.com/xiyanxiyan10/stocktrader/constant snack.com/xiyanxiyan10/stocktrader/api snack.com/xiyanxiyan10/stocktrader/config
